6a8fe32d8d3111e402a796ea507d966303bc5ad3
uploading1111000 <alexkdaniels@icloud.com>
2022-08-14 00:00:21 +0100

    stuff
    suck my nuts

---
+++ solver/FitnessFunctions.py
@@ -0,0 +1,15 @@
+def indexCoincidence(text):
+    frequencies = [0 for x in range(26)]
+    for x in text:
+        frequencies[x] += 1
+    sum = 0
+    for i in frequencies:
+        sum += i * (i - 1)
+    n = len(text)
+    return sum / (n * (n+1))
+    
+def getNGrams(text,n):
+    return [text[i:i+n] for i in range(len(text)-n + 1)]
+
+if __name__ == "__main__":
+    print ("suck my balls")
