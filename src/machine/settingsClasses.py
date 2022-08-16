4c2c9005dd7a225904a7b4fe30e1e9c630587fa0
uploading1111000 <alexkdaniels@icloud.com>
2022-08-17 00:52:50 +0100

    more availiable settings work
    added the M4 and M3 specification
    replaced the number of rotors with a number of rotors for quick access and a
    set of allowed positions for each "type" of rotor (used for m4)

--- src/machine/settingsClasses.py
+++ src/machine/settingsClasses.py
@@ -2,39 +2,62 @@ import json
 class availableSettings:
     #defaults to Enigma I specification
     rotorDefault = [
-    ("EKMFLGDQVZNTOWYHXUSPAIBRCJ","Q"), 
-    ("AJDKSIRUXBLHWTMCQGZNPYFVOE","E"), 
-    ("BDFHJLCPRTXVZNYEIWGAKMUSQO","V"),
-    ("ESOVPZJAYQUIRHXLNFTGKDCMWB","J"),
-    ("VZBRGITYUPSDNHLXAWMJQOFECK","Z")  
+    ("EKMFLGDQVZNTOWYHXUSPAIBRCJ","Q", 0), 
+    ("AJDKSIRUXBLHWTMCQGZNPYFVOE","E", 0), 
+    ("BDFHJLCPRTXVZNYEIWGAKMUSQO","V", 0),
+    ("ESOVPZJAYQUIRHXLNFTGKDCMWB","J", 0),
+    ("VZBRGITYUPSDNHLXAWMJQOFECK","Z", 0)  
     ]
     reflectorDefaults = [
         "YRUHQSLDPXNGOKMIEBFZCWVJAT",
         "FVPJIAOYEDRZXWGCTKUQSBNMHL"
         ] 
-    defaultNumRotors = 3
+    allowedDefault = [0,0,0] #defines which rotors can go where and doubles as specifying how many rotors there are
     def __init__(self, **kwargs):
-        self.rotors = availableSettings.rotorDefault
-        self.reflectors = availableSettings.reflectorDefaults
-        self.numRotors = availableSettings.defaultNumRotors
+        self.rotors = self.rotorDefault
+        self.reflectors = self.reflectorDefaults
+        self.allowed = self.allowedDefault
+        self.num = len(self.allowedDefault)
         if "filename" in kwargs.keys():
             self.getFromFile(kwargs["filename"])
         self.setFromArguments(**kwargs)
     
     def getFromFile(self, filename):
-        pass
-
+        #needs sanitation
+        with open(filename,"r") as f:
+            dict = json.load(f)
+        try:
+            self.rotors = dict["rotors"]
+            self.reflectors = dict["reflectors"]
+            self.numRotors = dict["allowed"]
+        except KeyError:
+            print("Invalid file! Maybe json formatting wrong or missing a specification")
+        if self.rotors == "default":
+            self.rotors = self.rotors
+        if self.reflectors == "default":
+            self.reflectors = self.reflectorDefaults
+        if self.allowed == "default":
+            self.allowed = self.allowedDefault
+            self.num = len(self.allowedDefault)
+ 
     def writeToFile(self, filename):
         writeable = {
             "rotors": self.rotors,
             "reflectors": self.reflectors,
-            "number": self.numRotors
+            "allowed": self.allowed
         }
         with open(filename,"w") as f:
             json.dump(writeable,f)
 
     def setFromArguments(self, **kwargs):
-        pass
+        for kwarg,value in kwargs.items():
+            if kwarg == "rotors":
+                self.rotors = value
+            elif kwarg == "reflectors":
+                self.reflectors = value
+            elif kwarg == "allowed":
+                self.allowed = value
+                self.num = len(value)
 
     def generateInternals(self):
         pass
--- src/main.py
+++ src/main.py
@@ -1,3 +1,1 @@
 from machine.settingsClasses import availableSettings,settings
-I = availableSettings()
-I.writeToFile("src/machineTemplates/enigmaI.json")
